5520cf1fe16ab84c6fa73cef15537c3987a9cbda
Jack <731547576@qq.com>
2018-11-08 07:10:28 +0000

    12

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,11 +10,11 @@
 | the IoC container for the system binding all of the various parts.
 |
 */
-
+require __DIR__.'/helpers.php';
 $app = new Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
 );
-require __DIR__.'/helpers.php';
+
 
 /*
 |--------------------------------------------------------------------------
--- config/database.php
+++ config/database.php
@@ -1,35 +1,10 @@
 <?php
 
+$db_config = get_db_config();
+
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Database Connection Name
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the database connections below you wish
-    | to use as your default connection for all database work. Of course
-    | you may use many connections at once using the Database library.
-    |
-    */
-
-    'default' => env('DB_CONNECTION', 'mysql'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Database Connections
-    |--------------------------------------------------------------------------
-    |
-    | Here are each of the database connections setup for your application.
-    | Of course, examples of configuring each database platform that is
-    | supported by Laravel is shown below to make development simple.
-    |
-    |
-    | All database work in Laravel is done through the PHP PDO facilities
-    | so make sure you have the driver for your particular database of
-    | choice installed on your machine before you begin development.
-    |
-    */
+    'default' => $db_config['connection'],
 
     'connections' => [
 
@@ -54,17 +29,30 @@ return [
             'engine' => null,
         ],
 
+        /*'pgsql' => [
+            'driver'   => 'pgsql',
+            'host'     => $db_config['host'],
+            'port'     => env('DB_PORT', '5432'),
+            'database' => $db_config['database'],
+            'username' => $db_config['username'],
+            'password' => $db_config['password'],
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+            'sslmode'  => 'prefer',
+        ],*/
+
         'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
+            'driver'   => 'pgsql',
+            'host'     => 'ec2-23-23-101-25.compute-1.amazonaws.com',
+            'port'     => '5432',
+            'database' => 'd409pulrpeq136',
+            'username' => 'tbeehfkskjrwra',
+            'password' => '4ffa2aa37e445636ef176f133ed096eac497ada263d7d4208e91966c5add816c',
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+            'sslmode'  => 'prefer',
         ],
 
         'sqlsrv' => [
